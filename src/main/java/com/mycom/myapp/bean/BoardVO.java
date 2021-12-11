7fb9afe61abee2512e14a8dfc0fcc175c3ffe1f0
junhyungparker <78070791+junhyungparker@users.noreply.github.com>
2021-12-11 16:06:54 +0900

    Update BoardVO.java

--- src/main/java/com/mycom/myapp/bean/BoardVO.java
+++ src/main/java/com/mycom/myapp/bean/BoardVO.java
@@ -3,24 +3,24 @@ package com.mycom.myapp.bean;
 import java.util.Date;
 
 public class BoardVO {
-	private int seq;
-	private String title;
+	private int id;
+	private String category;
 	private String writer;
+	private String birthDate;
 	private String content;
+	private String phone;
 	private Date regdate;
-	private int cnt;
-	
-	public int getSeq() {
-		return seq;
+	public int getId() {
+		return id;
 	}
-	public void setSeq(int seq) {
-		this.seq = seq;
+	public void setId(int id) {
+		this.id = id;
 	}
-	public String getTitle() {
-		return title;
+	public String getCategory() {
+		return category;
 	}
-	public void setTitle(String title) {
-		this.title = title;
+	public void setCategory(String category) {
+		this.category = category;
 	}
 	public String getWriter() {
 		return writer;
@@ -28,11 +28,23 @@ public class BoardVO {
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
 	public String getContent() {
 		return content;
 	}
 	public void setContent(String content) {
 		this.content = content;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 	public Date getRegdate() {
 		return regdate;
@@ -40,10 +52,6 @@ public class BoardVO {
 	public void setRegdate(Date regdate) {
 		this.regdate = regdate;
 	}
-	public int getCnt() {
-		return cnt;
-	}
-	public void setCnt(int cnt) {
-		this.cnt = cnt;
-	}
+	
+	
 }
